917b266bc7b20889bed0f4afc05d090b06e0ec0b
天哥 <@DESKTOP-L5AOSUS>
2019-12-03 15:35:55 +0800

    v2-fork

--- abc/src/abc/Test.java
+++ abc/src/abc/Test.java
@@ -3,5 +3,6 @@ package abc;
 public class Test {
       public void main(String args[]){
           System.out.println("hahaha");
+          getClass().asSubclass(null);
       }
 }
---
+++ abc/src/abc/Test2.java
@@ -0,0 +1,12 @@
+package abc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		List list =new ArrayList<>();
+		
+	}
+}
